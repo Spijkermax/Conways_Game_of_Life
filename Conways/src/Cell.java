4bf939670e81f7868ae41307eb5d219406bf786f
Oliver Denby <denbyoliv@ecs.vuw.ac.nz>
2020-09-28 22:00:16 +1300

    Update Cell.java

--- Conways/src/Cell.java
+++ Conways/src/Cell.java
@@ -1,22 +1,42 @@
+import java.util.ArrayList;
 
 public class Cell {
+	int x;
+	int y;
 	boolean alive;
 	String symbol;
-	
-	public Cell() {
+	private boolean shouldSwap;
+
+	public Cell(int x, int y) {
+		this.x = x;
+		this.y = y;
 		alive = false;
 		symbol = " ";
+
 		
-		Double d = Math.random();
-		if (d < 0.2) {
-			swapState();
-		}
 	}
-	
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
 	public boolean isAlive() {
 		return alive;
-		
+
 	}
+
 	
 	public void swapState() {
 		alive = !alive;
@@ -26,9 +46,85 @@ public class Cell {
 			symbol = " ";
 		}
 	}
-	
+
 	public String getSymbol() {
 		return symbol;
 	}
 
+	// return the number of living cells surrounding this cell
+	public int checkLiveNeighbours(CellWorld cw) {
+		int countLive = 0;
+		int counterY = 0;
+		int counterX = 0;
+		int iterate = 0;
+		for (int i = 1; i < 1 + 1; i++) {
+			iterate += i * 8;
+		}
+		iterate += 1;
+		
+		//this loops through and gets every x and y combination for the cells which surround THIS cell (x)
+		// [x-1y-1][x  y-1][x+1y-1]
+		// [x-1y  ][ this ][x+1y  ]
+		// [x-1y+1][x  y+1][x+1y+1]
+		// will also include itself so we must account for this at the end ***
+		
+		for (int i = 0; i < iterate; i++) {
+			int xx = x - (1 - counterX);
+			int yy = y - (1 - counterY);
+			
+			//try and catch here incase we try to get a cell using an x or y that is outside the 2d array height/width
+			try
+			{
+			if (cw.getCell(xx, yy).isAlive()) {
+				countLive++;
+			}
+			} catch (Exception e) {
+				
+			}
+
+			counterX++;
+			if (counterX == (1 * 2 + 1)) {
+				counterX = 0;
+				counterY++;
+			}
+
+		}
+		// *** here we account for the fact that it would add itself to the count if THIS.alive == true
+		if (this.isAlive()) {
+			countLive--;
+		}
+		return countLive;
+	}
+	
+	// this method defines the rules of the game and sets a boolean if it should change living state
+		// A live cell should become a dead cell if it has less than 2 or more than 3 surrounding live cells
+			// else it should remain a live cell
+		// A dead cell should become a live cell if it has 3 surrounding live cells
+			// else it should remain a dead cell
+	// shouldSwap boolean used so as to not change the living status of any cell before we have finished iterating
+	
+	public void shouldSwap(CellWorld cw) {
+		if (isAlive()) {
+			if (checkLiveNeighbours(cw) == 2 || checkLiveNeighbours(cw) == 3) {
+				return;
+			} else {
+				shouldSwap = true;
+			}
+		} else if (!isAlive()) {
+			if (checkLiveNeighbours(cw) == 3) {
+				shouldSwap = true;
+			}
+		}
+		
+	}
+	
+	public void update() {
+		if (shouldSwap) {
+			swapState();
+		}
+		// reset the boolean back to false for use on next CellWorld tick
+		shouldSwap = false;
+	}
+	
+	
 }
